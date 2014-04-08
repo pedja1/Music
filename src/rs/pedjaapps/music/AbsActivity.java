4cae1e7b3b342f3dab6f2aba2ef77b5ddfd900ef
pedja <predragcokulov@gmail.com>
2014-04-08 16:42:18 +0200

    added support v7 lib

---
+++ src/rs/pedjaapps/music/AbsActivity.java
@@ -0,0 +1,10 @@
+package rs.pedjaapps.music;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by pedja on 8.4.14..
+ */
+public class AbsActivity extends ActionBarActivity
+{
+}
